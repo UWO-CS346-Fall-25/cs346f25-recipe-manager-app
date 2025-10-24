b1383e1b795036504fc875f7ca42ecaa37e03189
Connor McGuire <connormm1020@gmail.com>
2025-10-24 15:42:57 -0500

    Added basic behavior to website on home-page.ejs and shopping-list.ejs

--- src/public/js/homeScript.js
+++ src/public/js/homeScript.js
@@ -4,6 +4,9 @@
 //
 document.getElementById("create-recipe-button").addEventListener("click", function(){
 
+    // Make it so text areas do not clear when invalid values entered.
+    preventDefault();
+
     let recipeBox = document.getElementById("recipe-name-input");
     let ingredientsBox = document.getElementById("ingredients-input");
     let notesBox = document.getElementById("notes-input");
