a2b1cebee479761e1cf24231448de401c8e7b084
Connor McGuire <connormm1020@gmail.com>
2025-12-06 00:39:24 -0600

    Website now double checks with user before deleting shopping list

--- src/public/js/shoppingListScript.js
+++ src/public/js/shoppingListScript.js
@@ -4,6 +4,15 @@
 //
 
 //
+// Make sure user actually wants to delete shopping list
+//
+document.getElementById("clear-list-button").addEventListener("click", function(e){
+    if (!confirm("Are you sure you want to delete your shopping list?")) {
+      e.preventDefault(); 
+    }
+  });
+
+//
 // Logout the user
 //
 document.getElementById('log-out-button').addEventListener('click', function(){
